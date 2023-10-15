149f52070d3511e00cd4d89d5c74a450ebbd7b45
Igor Liakh <igorlyah060@gmail.com>
2023-10-15 22:05:17 +0300

    added imgs

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,22 +1,20 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector(".close-modal"),
-    backdrop: document.querySelector("[data-modal-backdrop]"),
-    modal: document.querySelector(".modal"),
-  };
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('.close-modal'),
+  backdrop: document.querySelector('[data-modal-backdrop]'),
+  modal: document.querySelector('.modal'),
+};
 
-  refs.openModalBtn.addEventListener("click", openModal);
-  refs.closeModalBtn.addEventListener("click", closeModal);
-  refs.backdrop.addEventListener("click", closeModal);
+refs.openModalBtn.addEventListener('click', openModal);
+refs.closeModalBtn.addEventListener('click', closeModal);
+refs.backdrop.addEventListener('click', closeModal);
 
-  function openModal() {
-    refs.modal.classList.remove("is-hidden");
-    refs.backdrop.classList.remove("is-hidden");
-  }
+function openModal() {
+  refs.modal.classList.remove('is-hidden');
+  refs.backdrop.classList.remove('is-hidden');
+}
 
-  function closeModal() {
-    refs.modal.classList.add("is-hidden");
-    refs.backdrop.classList.add("is-hidden");
-  }
-})();
+function closeModal() {
+  refs.modal.classList.add('is-hidden');
+  refs.backdrop.classList.add('is-hidden');
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 import './js/widget';
+import './js/modal';
