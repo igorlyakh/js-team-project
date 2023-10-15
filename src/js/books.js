06a59828af42058ad1fecf30d32da42814833735
Sonya1488 <127775334+Sofiia-28@users.noreply.github.com>
2023-10-15 18:39:12 +0300

    home done

--- src/js/books.js
+++ src/js/books.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const categoryList = document.querySelector('.category-list');
 const allCategories = document.getElementById('all-categories');
 const bestSellers = document.querySelector('.best-sellers');
 const bestSellersList = document.querySelector('.best-sellers-list');
-const booksList = document.querySelector('.books-list');
-const mediaTablet = window.matchMedia('(min-width: 768px) and (max-width: 1280px)');
+const booksListContainer = document.querySelector('.books-list-container');
+const mediaTablet = window.matchMedia(
+  '(min-width: 768px) and (max-width: 1280px)'
+);
 const mediaDesktop = window.matchMedia('(min-width: 1280px)');
 
-console.log(mediaDesktop.matches);
-
 const URL = 'https://books-backend.p.goit.global';
+
+// Function for the list of categories
 
 async function getCategoryList() {
   try {
@@ -19,37 +22,40 @@ async function getCategoryList() {
       'beforeend',
       createCategoryListMarkup(data)
     );
-    allCategories.style.color = '#4f2ee8';
-    allCategories.style.textTransform = 'uppercase';
-    allCategories.style.fontWeight = '700';
-    // console.log(data);
+
+    selectedStyle(allCategories);
   } catch (err) {
     console.log('TRY-CATCH:', err);
+    Notiflix.Notify.failure('This page is empty, please try again');
   }
 }
 getCategoryList();
 
+// Function for the list of BEST SELLERS BOOKS
 allCategories.addEventListener('click', getBestSellersBooks);
 
 async function getBestSellersBooks() {
   try {
     const { data } = await axios.get(`${URL}/books/top-books`);
     bestSellers.style.display = 'block';
-    booksList.innerHTML = '';
+    booksListContainer.innerHTML = '';
     bestSellersList.insertAdjacentHTML(
       'beforeend',
       createBestSellersMarkup(data)
     );
+
     for (const child of bestSellersList.children) {
       const btn = child.lastElementChild;
       btn.addEventListener('click', getBooksListByBtn);
     }
-    // console.log(data);
   } catch (err) {
     console.log('TRY-CATCH:', err);
+    Notiflix.Notify.failure('This page is empty, please try again');
   }
 }
 getBestSellersBooks();
+
+// Function for the list of BOOKS of each CATEGORY
 
 categoryList.addEventListener('click', getBooksList);
 async function getBooksList(event) {
@@ -59,69 +65,79 @@ async function getBooksList(event) {
       child.removeAttribute('style');
     }
     const category = event.target;
-    category.style.color = '#4f2ee8';
-    category.style.textTransform = 'uppercase';
-    category.style.fontWeight = '700';
+    selectedStyle(category);
 
     const { data } = await axios.get(
       `${URL}/books/category?category=${category.textContent}`
     );
     bestSellers.style.display = 'none';
-    booksList.innerHTML = '';
-    if (booksList.previousElementSibling.tagName === "H1") {
-    booksList.previousElementSibling.innerHTML = ''; 
-    }
-    booksList.insertAdjacentHTML('beforeend', createBooksListMarkup(data));
-    booksList.insertAdjacentHTML(
-      'beforebegin',
-      `<h1 class="title">${category.textContent}</h1>`
+    booksListContainer.innerHTML = '';
+
+    const arrFromTitle = category.textContent.trim().split(' ');
+    const lastWordOfTitle = arrFromTitle.pop();
+    booksListContainer.insertAdjacentHTML(
+      'afterbegin',
+      `<h1 class="title">${arrFromTitle.join(
+        ' '
+      )} <span class="title-accent">${lastWordOfTitle}</span></h1>
+        <ul class="books-list"></ul>`
     );
+
+    const booksList = document.querySelector('.books-list');
+    booksList.insertAdjacentHTML('beforeend', createBooksListMarkup(data));
+
     if (mediaTablet.matches === true || mediaDesktop.matches === true) {
       booksList.className = 'books-list-tab-desk';
-    };
-    // console.log(data);
+    }
   } catch (err) {
     console.log('TRY-CATCH:', err);
+    Notiflix.Notify.failure('This page is empty, please try again');
   }
 }
+
+// Function for the list of BOOKS of each category BY BUTTON
 
 async function getBooksListByBtn(event) {
   event.preventDefault();
   try {
     const currentCategory = event.currentTarget.id;
-    const category = categoryList.children;
-    for (const child of category) {
-      child.removeAttribute('style');
-      // console.log(child);
-      // if(child.textContent === currentCategory){
-      //     child.style.color = "#4f2ee8";
-      //     child.style.textTransform = "uppercase";
-      //     child.style.fontWeight = "700";
-      // }
+    const categories = categoryList.children;
+    for (const category of categories) {
+      category.removeAttribute('style');
+      if (category.textContent.trim() === currentCategory.trim()) {
+        selectedStyle(category);
+      }
     }
 
     const { data } = await axios.get(
       `${URL}/books/category?category=${currentCategory}`
     );
     bestSellers.style.display = 'none';
-    booksList.innerHTML = '';
-    if (booksList.previousElementSibling.tagName === "H1") {
-        booksList.previousElementSibling.innerHTML = ''; 
-        }
-    booksList.insertAdjacentHTML(
-      'beforebegin',
-      `<h1 class="title">${currentCategory}</h1>`
+    booksListContainer.innerHTML = '';
+
+    const arrFromTitle = currentCategory.trim().split(' ');
+    const lastWordOfTitle = arrFromTitle.pop();
+    booksListContainer.insertAdjacentHTML(
+      'afterbegin',
+      `<h1 class="title">${arrFromTitle.join(
+        ' '
+      )} <span class="title-accent">${lastWordOfTitle}</span></h1>
+      <ul class="books-list"></ul>`
     );
+    const booksList = document.querySelector('.books-list');
     booksList.insertAdjacentHTML('beforeend', createBooksListMarkup(data));
     window.scrollTo(0, 0);
+
     if (mediaTablet.matches === true || mediaDesktop.matches === true) {
       booksList.className = 'books-list-tab-desk';
     }
-    // console.log(data);
   } catch (err) {
     console.log('TRY-CATCH:', err);
+    Notiflix.Notify.failure('This page is empty, please try again');
   }
 }
+
+// Functions for creating MARKUPS
 
 function createCategoryListMarkup(categories) {
   return categories
@@ -140,18 +156,18 @@ function createBestSellersMarkup(categories) {
         ({ list_name, books }) =>
           `<li class="books-container-tab-desk"><h2 class="book-category">${list_name}</h2>
           <ul class="books-list-tab-desk">
-          <li class="book-card">
-    <img class="book-image" src="${books[0].book_image}" alt="${books[0].title} book cover" />
+          <li class="book-card" data-modal-open>
+    <img class="book-image" width="218" height="316" src="${books[0].book_image}" alt="${books[0].title} book cover" />
     <h3 class="book-name">${books[0].title}</h3>
     <p class="book-author">${books[0].author}</p>
     </li>
-    <li class="book-card">
-    <img class="book-image" src="${books[1].book_image}" alt="${books[0].title} book cover" />
+    <li class="book-card" data-modal-open>
+    <img class="book-image" width="218" height="316" src="${books[1].book_image}" alt="${books[0].title} book cover" />
     <h3 class="book-name">${books[1].title}</h3>
     <p class="book-author">${books[1].author}</p>
     </li>
-    <li class="book-card">
-    <img class="book-image" src="${books[2].book_image}" alt="${books[0].title} book cover" />
+    <li class="book-card" data-modal-open>
+    <img class="book-image" width="218" height="316" src="${books[2].book_image}" alt="${books[0].title} book cover" />
     <h3 class="book-name">${books[2].title}</h3>
     <p class="book-author">${books[2].author}</p>
     </li></ul>
@@ -164,28 +180,28 @@ function createBestSellersMarkup(categories) {
         ({ list_name, books }) =>
           `<li class="books-container-tab-desk"><h2 class="book-category">${list_name}</h2>
           <ul class="books-list-tab-desk">
-          <li class="book-card">
-    <img class="book-image" src="${books[0].book_image}" alt="${books[0].title} book cover" />
+          <li class="book-card" data-modal-open>
+    <img class="book-image" width="180" height="256" src="${books[0].book_image}" alt="${books[0].title} book cover" />
     <h3 class="book-name">${books[0].title}</h3>
     <p class="book-author">${books[0].author}</p>
     </li>
-    <li class="book-card">
-    <img class="book-image" src="${books[1].book_image}" alt="${books[0].title} book cover" />
+    <li class="book-card" data-modal-open>
+    <img class="book-image" width="180" height="256" src="${books[1].book_image}" alt="${books[0].title} book cover" />
     <h3 class="book-name">${books[1].title}</h3>
     <p class="book-author">${books[1].author}</p>
     </li>
-    <li class="book-card">
-    <img class="book-image" src="${books[2].book_image}" alt="${books[0].title} book cover" />
+    <li class="book-card" data-modal-open>
+    <img class="book-image" width="180" height="256" src="${books[2].book_image}" alt="${books[0].title} book cover" />
     <h3 class="book-name">${books[2].title}</h3>
     <p class="book-author">${books[2].author}</p>
     </li>
-    <li class="book-card">
-    <img class="book-image" src="${books[3].book_image}" alt="${books[0].title} book cover" />
+    <li class="book-card" data-modal-open>
+    <img class="book-image" width="180" height="256" src="${books[3].book_image}" alt="${books[0].title} book cover" />
     <h3 class="book-name">${books[3].title}</h3>
     <p class="book-author">${books[3].author}</p>
     </li>
-    <li class="book-card">
-    <img class="book-image" src="${books[4].book_image}" alt="${books[0].title} book cover" />
+    <li class="book-card" data-modal-open>
+    <img class="book-image" width="180" height="256" src="${books[4].book_image}" alt="${books[0].title} book cover" />
     <h3 class="book-name">${books[4].title}</h3>
     <p class="book-author">${books[4].author}</p>
     </li></ul>
@@ -196,9 +212,9 @@ function createBestSellersMarkup(categories) {
     return categories
       .map(
         ({ list_name, books }) =>
-          `<li class="book-card">
+          `<li class="book-card" data-modal-open>
     <h2 class="book-category">${list_name}</h2>
-    <img class="book-image" src="${books[0].book_image}" alt="${books[0].title} book cover" />
+    <img class="book-image" width="335" height="485" src="${books[0].book_image}" alt="${books[0].title} book cover" />
     <h3 class="book-name">${books[0].title}</h3>
     <p class="book-author">${books[0].author}</p>
     <button class="see-more-btn" id="${list_name}" type="button">See more</button>
@@ -209,14 +225,45 @@ function createBestSellersMarkup(categories) {
 }
 
 function createBooksListMarkup(books) {
-  return books
-    .map(
-      ({ book_image, title, author }) =>
-        `<div class="book-card">
-    <img class="book-image" src="${book_image}" alt="${title} book cover" />
+  if (mediaDesktop.matches === true) {
+    return books
+      .map(
+        ({ book_image, title, author }) =>
+          `<li class="book-card" data-modal-open>
+    <img class="book-image" width="180" height="256" src="${book_image}" alt="${title} book cover" />
     <h2 class="book-name">${title}</h2>
     <p class="book-author">${author}</p>
-    </div>`
-    )
-    .join('');
+    </li>`
+      )
+      .join('');
+  } else if (mediaTablet.matches === true) {
+    return books
+      .map(
+        ({ book_image, title, author }) =>
+          `<li class="book-card" data-modal-open>
+<img class="book-image" width="218" height="316" src="${book_image}" alt="${title} book cover" />
+<h2 class="book-name">${title}</h2>
+<p class="book-author">${author}</p>
+</li>`
+      )
+      .join('');
+  } else {
+    return books
+      .map(
+        ({ book_image, title, author }) =>
+          `<li class="book-card" data-modal-open>
+    <img class="book-image" width="335" height="485" src="${book_image}" alt="${title} book cover" />
+    <h2 class="book-name">${title}</h2>
+    <p class="book-author">${author}</p>
+    </li>`
+      )
+      .join('');
+  }
+}
+
+// Function for style of selected category
+function selectedStyle(categoryName) {
+  return (categoryName.style.cssText = `color: #4f2ee8;
+   text-transform: uppercase;
+   font-weight: 700;`);
 }
