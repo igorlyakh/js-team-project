fc05198c65527a4b230a6f0e18bcfd6d983d2770
heorh11 <gheorghe.filip225@gmail.com>
2023-10-19 03:08:31 +0300

    1212

--- src/js/books.js
+++ src/js/books.js
@@ -30,13 +30,13 @@ if (path[path.length - 1] === '') {
       for (const child of categoryList.children) {
         child.removeAttribute('style');
       }
-    selectedStyle(allCategories);
-  } catch (err) {
-    console.log('TRY-CATCH:', err);
-    Notiflix.Notify.failure('This page is empty, please try again');
+      selectedStyle(allCategories);
+    } catch (err) {
+      console.log('TRY-CATCH:', err);
+      Notiflix.Notify.failure('This page is empty, please try again');
+    }
   }
-}
-getCategoryList();
+  getCategoryList();
 
   // Function for the list of BEST SELLERS BOOKS
   allCategories.addEventListener('click', getBestSellersBooks);
@@ -51,16 +51,16 @@ getCategoryList();
         createBestSellersMarkup(data)
       );
 
-    for (const child of bestSellersList.children) {
-      const btn = child.lastElementChild;
-      btn.addEventListener('click', getBooksListByBtn);
+      for (const child of bestSellersList.children) {
+        const btn = child.lastElementChild;
+        btn.addEventListener('click', getBooksListByBtn);
+      }
+    } catch (err) {
+      console.log('TRY-CATCH:', err);
+      Notiflix.Notify.failure('This page is empty, please try again');
     }
-  } catch (err) {
-    console.log('TRY-CATCH:', err);
-    Notiflix.Notify.failure('This page is empty, please try again');
   }
-}
-getBestSellersBooks();
+  getBestSellersBooks();
 
   // Function for the list of BOOKS of each CATEGORY
 
@@ -98,60 +98,60 @@ getBestSellersBooks();
       const booksList = document.querySelector('.books-list');
       booksList.insertAdjacentHTML('beforeend', createBooksListMarkup(data));
 
-    if (mediaTablet.matches === true || mediaDesktop.matches === true) {
-      booksList.className = 'books-list-tab-desk';
+      if (mediaTablet.matches === true || mediaDesktop.matches === true) {
+        booksList.className = 'books-list-tab-desk';
+      }
+    } catch (err) {
+      console.log('TRY-CATCH:', err);
+      Notiflix.Notify.failure('This page is empty, please try again');
     }
-  } catch (err) {
-    console.log('TRY-CATCH:', err);
-    Notiflix.Notify.failure('This page is empty, please try again');
   }
-}
 
   // Function for the list of BOOKS of each category BY BUTTON
 
-//   async function getBooksListByBtn(event) {
-//     event.preventDefault();
-//     try {
-//       const currentCategory = event.currentTarget.id;
-//       const categories = categoryList.children;
-//       for (const category of categories) {
-//         category.removeAttribute('style');
-//         if (category.textContent.trim() === currentCategory.trim()) {
-//           selectedStyle(category);
-//           const theme = localStorage.getItem('theme');
-//           if (theme === 'dark') {
-//             selectedStyleBlack(category);
-//           }
-//         }
-//       }
+  async function getBooksListByBtn(event) {
+    event.preventDefault();
+    try {
+      const currentCategory = event.currentTarget.id;
+      const categories = categoryList.children;
+      for (const category of categories) {
+        category.removeAttribute('style');
+        if (category.textContent.trim() === currentCategory.trim()) {
+          selectedStyle(category);
+          const theme = localStorage.getItem('theme');
+          if (theme === 'dark') {
+            selectedStyleBlack(category);
+          }
+        }
+      }
 
-//       const { data } = await axios.get(
-//         `${URL}/books/category?category=${currentCategory}`
-//       );
-//       bestSellers.style.display = 'none';
-//       booksListContainer.innerHTML = '';
+      const { data } = await axios.get(
+        `${URL}/books/category?category=${currentCategory}`
+      );
+      bestSellers.style.display = 'none';
+      booksListContainer.innerHTML = '';
 
-//       const arrFromTitle = currentCategory.trim().split(' ');
-//       const lastWordOfTitle = arrFromTitle.pop();
-//       booksListContainer.insertAdjacentHTML(
-//         'afterbegin',
-//         `<h1 class="title">${arrFromTitle.join(
-//           ' '
-//         )} <span class="title-accent">${lastWordOfTitle}</span></h1>
-//       <ul class="books-list"></ul>`
-//       );
-//       const booksList = document.querySelector('.books-list');
-//       booksList.insertAdjacentHTML('beforeend', createBooksListMarkup(data));
-//       window.scrollTo(0, 0);
+      const arrFromTitle = currentCategory.trim().split(' ');
+      const lastWordOfTitle = arrFromTitle.pop();
+      booksListContainer.insertAdjacentHTML(
+        'afterbegin',
+        `<h1 class="title">${arrFromTitle.join(
+          ' '
+        )} <span class="title-accent">${lastWordOfTitle}</span></h1>
+      <ul class="books-list"></ul>`
+      );
+      const booksList = document.querySelector('.books-list');
+      booksList.insertAdjacentHTML('beforeend', createBooksListMarkup(data));
+      window.scrollTo(0, 0);
 
-//     if (mediaTablet.matches === true || mediaDesktop.matches === true) {
-//       booksList.className = 'books-list-tab-desk';
-//     }
-//   } catch (err) {
-//     console.log('TRY-CATCH:', err);
-//     Notiflix.Notify.failure('This page is empty, please try again');
-//   }
-// }
+      if (mediaTablet.matches === true || mediaDesktop.matches === true) {
+        booksList.className = 'books-list-tab-desk';
+      }
+    } catch (err) {
+      console.log('TRY-CATCH:', err);
+      Notiflix.Notify.failure('This page is empty, please try again');
+    }
+  }
 
   // Functions for creating MARKUPS
 
@@ -165,140 +165,121 @@ getBestSellersBooks();
       .join('');
   }
 
-function createBestSellersMarkup(categories) {
-  if (mediaTablet.matches === true) {
-    return categories
-      .map(
-        ({ list_name, books }) =>
-          `<li class="books-container-tab-desk"><h2 class="book-category">${list_name}</h2>
-          <ul class="books-list-tab-desk">
-          <li class="book-card" data-id="${books[0]._id}><div data-modal> 
-    <img class="book-image" width="218" height="316" src="${books[0].book_image}" alt="${books[0].title} book cover" />
-    <h3 class="book-name">${books[0].title}</h3>
-    <p class="book-author">${books[0].author}</p>
-    </div>
-    </li>
-    <li class="book-card"  data-id="${books[1]._id}><div data-modal>  
-    <img class="book-image" width="218" height="316" src="${books[1].book_image}" alt="${books[0].title} book cover" />
-    <h3 class="book-name">${books[1].title}</h3>
-    <p class="book-author">${books[1].author}</p>
-    </div>
-    </li>
-    <li class="book-card" data-id='${books[2]._id}><div data-modal> 
-    <img class="book-image" width="218" height="316" src="${books[2].book_image}" alt="${books[0].title} book cover" />
-    <h3 class="book-name">${books[2].title}</h3>
-    <p class="book-author">${books[2].author}</p>
-    </div>
-
-    </li></ul>
-    <button class="see-more-btn" id="${list_name}" type="button">See more</button></li>`
-      )
-      .join('');
-  } else if (mediaDesktop.matches === true) {
-    return categories
-      .map(
-        ({ list_name, books }) =>
-          `<li class="books-container-tab-desk"><h2 class="book-category">${list_name}</h2>
-          <ul class="books-list-tab-desk">
-          <li class="book-card"  data-id='${books[0]._id}><div data-modal> 
-    <img class="book-image" width="180" height="256" src="${books[0].book_image}" alt="${books[0].title} book cover" />
-    <h3 class="book-name">${books[0].title}</h3>
-    <p class="book-author">${books[0].author}</p>
-    </div>
-    </li>
-    <li class="book-card" data-id='${books[1]._id}><div data-modal> 
-    <img class="book-image" width="180" height="256" src="${books[1].book_image}" alt="${books[0].title} book cover" />
-    <h3 class="book-name">${books[1].title}</h3>
-    <p class="book-author">${books[1].author}</p>
-    </div>
-    </li>
-    <li class="book-card"  data-id='${books[2]._id}><div data-modal> 
-    <img class="book-image" width="180" height="256" src="${books[2].book_image}" alt="${books[0].title} book cover" />
-    <h3 class="book-name">${books[2].title}</h3>
-    <p class="book-author">${books[2].author}</p>
-    </div>
-    </li>
-    <li class="book-card" data-id='${books[3]._id}><div data-modal> 
-    <img class="book-image" width="180" height="256" src="${books[3].book_image}" alt="${books[0].title} book cover" />
-    <h3 class="book-name">${books[3].title}</h3>
-    <p class="book-author">${books[3].author}</p>
-    </div>
-    </li>
-    <li class="book-card" data-id='${books[4]._id}><div data-modal> 
-    <img class="book-image" width="180" height="256" src="${books[4].book_image}" alt="${books[0].title} book cover" />
-    <h3 class="book-name">${books[4].title}</h3>
-    <p class="book-author">${books[4].author}</p>
-    </div>
-    </li></ul>
-    <button class="see-more-btn" id="${list_name}" type="button">See more</button></li>`
-      )
-      .join('');
-  } else {
-    return categories
-      .map(
-        ({ list_name, books }) =>
-          `<li class="book-card" data-id='${books[0]._id}><div data-modal> 
-    <h2 class="book-category">${list_name}</h2>
-    <img class="book-image" width="335" height="485" src="${books[0].book_image}" alt="${books[0].title} book cover" />
-    <h3 class="book-name">${books[0].title}</h3>
-    <p class="book-author">${books[0].author}</p>
-    <button class="see-more-btn" id="${list_name}" type="button">See more</button>
-    </div>
-    </li>`
-        )
+  function createBestSellersMarkup(categories) {
+    if (mediaTablet.matches === true) {
+      return categories
+        .map(({ list_name, books }) => {
+          return `<li class="books-container-tab-desk"><h2 class="book-category">${list_name}</h2>
+        <ul class="books-list-tab-desk">
+        <li class="book-card" data-id="${books[0]._id}"><div data-modal> 
+          <img class="book-image" width="218" height="316" src="${books[0].book_image}" alt="${books[0].title} book cover" />
+          <h3 class="book-name">${books[0].title}</h3>
+          <p class="book-author">${books[0].author}</p>
+        </div>
+        </li>
+        <li class="book-card"  data-id="${books[1]._id}"><div data-modal>  
+          <img class="book-image" width="218" height="316" src="${books[1].book_image}" alt="${books[1].title} book cover" />
+          <h3 class="book-name">${books[1].title}</h3>
+          <p class="book-author">${books[1].author}</p>
+        </div>
+        </li>
+        <li class="book-card" data-id="${books[2]._id}"><div data-modal> 
+          <img class="book-image" width="218" height="316" src="${books[2].book_image}" alt="${books[2].title} book cover" />
+          <h3 class="book-name">${books[2].title}</h3>
+          <p class="book-author">${books[2].author}</p>
+        </div>
+        </li></ul>
+        <button class="see-more-btn" id="${list_name}" type="button">See more</button></li>`;
+        })
+        .join('');
+    } else if (mediaDesktop.matches === true) {
+      return categories
+        .map(({ list_name, books }) => {
+          return `<li class="books-container-tab-desk"><h2 class="book-category">${list_name}</h2>
+        <ul class="books-list-tab-desk">
+        <li class="book-card"  data-id="${books[0]._id}"><div data-modal> 
+          <img class="book-image" width="180" height="256" src="${books[0].book_image}" alt="${books[0].title} book cover" />
+          <h3 class="book-name">${books[0].title}</h3>
+          <p class="book-author">${books[0].author}</p>
+        </div>
+        </li>
+        <li class="book-card" data-id="${books[1]._id}"><div data-modal> 
+          <img class="book-image" width="180" height="256" src="${books[1].book_image}" alt="${books[1].title} book cover" />
+          <h3 class="book-name">${books[1].title}</h3>
+          <p class="book-author">${books[1].author}</p>
+        </div>
+        </li>
+        <li class="book-card"  data-id="${books[2]._id}"><div data-modal> 
+          <img class="book-image" width="180" height="256" src="${books[2].book_image}" alt="${books[2].title} book cover" />
+          <h3 class="book-name">${books[2].title}</h3>
+          <p class="book-author">${books[2].author}</p>
+        </div>
+        </li>
+        <li class="book-card" data-id="${books[3]._id}"><div data-modal> 
+          <img class="book-image" width="180" height="256" src="${books[3].book_image}" alt="${books[3].title} book cover" />
+          <h3 class="book-name">${books[3].title}</h3>
+          <p class="book-author">${books[3].author}</p>
+        </div>
+        </li>
+        <li class="book-card" data-id="${books[4]._id}"><div data-modal> 
+          <img class="book-image" width="180" height="256" src="${books[4].book_image}" alt="${books[4].title} book cover" />
+          <h3 class="book-name">${books[4].title}</h3>
+          <p class="book-author">${books[4].author}</p>
+        </div>
+        </li></ul>
+        <button class="see-more-btn" id="${list_name}" type="button">See more</button></li>`;
+        })
+        .join('');
+    } else {
+      return categories
+        .map(({ list_name, books }) => {
+          return `<li class="book-card" data-id="${books[0]._id}"><div data-modal> 
+        <h2 class="book-category">${list_name}</h2>
+        <img class="book-image" width="335" height="485" src="${books[0].book_image}" alt="${books[0].title} book cover" />
+        <h3 class="book-name">${books[0].title}</h3>
+        <p class="book-author">${books[0].author}</p>
+        <button class="see-more-btn" id="${list_name}" type="button">See more</button>
+        </div>
+        </li>`;
+        })
         .join('');
     }
   }
 
-function createBooksListMarkup(books) {
-  if (mediaDesktop.matches === true) {
-    return books
-      .map(
-        ({ book_image, title, author }) =>
-          `<li class="book-card" data-id='${book._id}> <div data-modal> 
-    <img class="book-image" width="180" height="256" src="${book_image}" alt="${title} book cover" />
-    <h2 class="book-name">${title}</h2>
-    <p class="book-author">${author}</p>
-    </div>
-    </li>`
-      )
-      .join('');
-  } else if (mediaTablet.matches === true) {
-    return books
-      .map(
-        ({ book_image, title, author }) =>
-          `<li class="book-card" data-id='${book._id}><div data-modal> 
-<img class="book-image" width="218" height="316" src="${book_image}" alt="${title} book cover" />
-<h2 class="book-name">${title}</h2>
-<p class="book-author">${author}</p>
-</div>
-</li>`
-      )
-      .join('');
-  } else {
-    return books
-      .map(
-        ({ book_image, title, author }) =>
-          `<li class="book-card" data-id='${book._id}><div data-modal> 
-    <img class="book-image" width="335" height="485" src="${book_image}" alt="${title} book cover" />
-    <h2 class="book-name">${title}</h2>
-    <p class="book-author">${author}</p>
-    </div> 
-    </li>`
-        )
+  function createBooksListMarkup(books) {
+    if (mediaDesktop.matches === true) {
+      return books
+        .map(({ book_image, title, author }) => {
+          return `<li class="book-card" data-id="${book._id}"> <div data-modal> 
+        <img class="book-image" width="180" height="256" src="${book_image}" alt="${title} book cover" />
+        <h2 class="book-name">${title}</h2>
+        <p class="book-author">${author}</p>
+        </div>
+        </li>`;
+        })
+        .join('');
+    } else if (mediaTablet.matches === true) {
+      return books
+        .map(({ book_image, title, author }) => {
+          return `<li class="book-card" data-id="${book._id}"><div data-modal> 
+        <img class="book-image" width="218" height="316" src="${book_image}" alt="${title} book cover" />
+        <h2 class="book-name">${title}</h2>
+        <p class "book-author">${author}</p>
+        </div>
+        </li>`;
+        })
+        .join('');
+    } else {
+      return books
+        .map(({ book_image, title, author }) => {
+          return `<li class="book-card" data-id="${book._id}"><div data-modal> 
+        <img class="book-image" width="335" height="485" src="${book_image}" alt="${title} book cover" />
+        <h2 class="book-name">${title}</h2>
+        <p class="book-author">${author}</p>
+        </div> 
+        </li>`;
+        })
         .join('');
     }
-  }
-
-  // Function for style of selected category
-  function selectedStyle(categoryName) {
-    return (categoryName.style.cssText = `color: #4f2ee8;
-   text-transform: uppercase;
-   font-weight: 700;`);
-  }
-  function selectedStyleBlack(categoryName) {
-    return (categoryName.style.cssText = `color: #EAC645;
-   text-transform: uppercase;
-   font-weight: 700;`);
   }
 }
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,78 +1,91 @@
 import axios from 'axios';
-document.addEventListener('DOMContentLoaded', () => {
-  
 
-  const openModal = document.querySelector('.best-sellers-list');
-  const booksList = document.querySelector('.books-list');
-  const modalBackdrop = document.querySelector('.modal-backdrop');
-  const closeButton = document.querySelector('.close-button');
+const modalBackdrop = document.querySelector('.modal-backdrop');
+const closeButton = document.querySelector('.close-button');
+const addToShoppingListButton = document.querySelector('.add-to-list');
+const DATA_KEY = 'users-book';
 
-  const refs = {
-    title: document.querySelector('.book-title'),
-    author: document.querySelector('.book-author'),
-    dscr: document.querySelector('.book-description'),
-    img: document.querySelector('.book-cover'),
-  };
-  
-   
-    document.addEventListener('click', (e) => {
-      if (e.target.dataset.modal) {
-      
-        modalBackdrop.style.display = 'flex';
-      }
-    });
+const refs = {
+  title: document.querySelector('.book-title'),
+  author: document.querySelector('.book-author'),
+  dscr: document.querySelector('.book-description'),
+  img: document.querySelector('.book-cover'),
+};
 
-    
-    closeButton.addEventListener('click', () => {
-      
-      modalBackdrop.style.display = 'none';
-    });
+function openModalWithData(res) {
+  refs.img.setAttribute('src', res.book_image);
+  refs.title.innerHTML = res.title;
+  refs.author.innerHTML = res.author;
+  refs.dscr.innerHTML = res.description || ' ';
+  modalBackdrop.style.display = 'flex';
 
-    // Add an event listener to the modal backdrop
-    modalBackdrop.addEventListener('click', (e) => {
-      // Check if the click occurred outside the modal container
-      if (e.target === modalBackdrop) {
-        // Hide the modal
-        modalBackdrop.style.display = 'none';
-      }
-    });
+  // Check if the book is already in the shopping list
+  const existingData = JSON.parse(localStorage.getItem(DATA_KEY)) || [];
+  const isBookInList = existingData.some((item) => item._id === res._id);
 
-    
-    const commonListContainer = document.querySelector('.home');
+  // Set the initial button text
+  if (isBookInList) {
+    addToShoppingListButton.innerHTML = 'Remove from Shopping List';
+  } else {
+    addToShoppingListButton.innerHTML = 'Add to Shopping List';
+  }
 
-  commonListContainer.addEventListener('click', (e) => {
-    const targetBookCard = e.target.closest('li.book-card');
-    if (targetBookCard) {
-      const bookId = targetBookCard.getAttribute('data-id'); 
-      console.log('Clicked ID:', bookId); 
-      getBookById(bookId).then((res) => {
-        console.log(res);
-        refs.img.setAttribute('src', res.book_image);
-        refs.title.innerHTML = res.list_name;
-        refs.author.innerHTML = res.author;
-        if (res.description.length === 0) {
-          refs.dscr.innerHTML = 'Нет описания';
-        } else {
-          refs.dscr.innerHTML = res.description;
-        }
-      });
+  // Add a click event listener to the "Add to Shopping List" button
+  addToShoppingListButton.addEventListener('click', () => {
+    if (isBookInList) {
+      // If the book is in the list, remove it
+      removeBookFromList(res._id);
+      addToShoppingListButton.innerHTML = 'Add to Shopping List';
+      alert('Book removed from your shopping list!');
+    } else {
+      // If the book is not in the list, add it
+      addToLocalStorage(res);
+      addToShoppingListButton.innerHTML = 'Remove from Shopping List';
+      alert('Book added to your shopping list!');
     }
   });
+}
 
+function closeModal() {
+  modalBackdrop.style.display = 'none';
+}
 
-    async function getBookById(id) {
-      const data = await axios.get(
-        `https://books-backend.p.goit.global/books/${id}`
-      );
-      const res = await data.data;
-      return res;
+document.addEventListener('click', async (e) => {
+  const targetBookCard = e.target.closest('[data-id]');
+  if (targetBookCard) {
+    const bookId = targetBookCard.getAttribute('data-id');
+    try {
+      const res = await getBookById(bookId);
+      openModalWithData(res);
+    } catch (error) {
+      console.error(error);
     }
-
+  }
 });
-  
 
+closeButton.addEventListener('click', () => {
+  closeModal();
+});
 
+modalBackdrop.addEventListener('click', (e) => {
+  if (e.target === modalBackdrop) {
+    closeModal();
+  }
+});
 
+async function getBookById(id) {
+  const data = await axios.get(`https://books-backend.p.goit.global/books/${id}`);
+  return data.data;
+}
 
+function addToLocalStorage(bookData) {
+  const existingData = JSON.parse(localStorage.getItem(DATA_KEY)) || [];
+  existingData.push(bookData);
+  localStorage.setItem(DATA_KEY, JSON.stringify(existingData));
+}
 
-  
+function removeBookFromList(bookId) {
+  const existingData = JSON.parse(localStorage.getItem(DATA_KEY)) || [];
+  const updatedData = existingData.filter((item) => item._id !== bookId);
+  localStorage.setItem(DATA_KEY, JSON.stringify(updatedData));
+}
